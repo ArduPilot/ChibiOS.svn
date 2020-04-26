0cdbf51d723491a20aba3e716f7a2085c7988eb6
gdisirio <gdisirio@27425a3e-05d8-49a3-a47f-9c15f0e5edd8>
2020-04-26 10:05:01 +0000

    git-svn-id: svn+ssh://svn.osdn.net/svnroot/chibios/branches/chibistudio_trunk@13569 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- tools/makelaunch/makelaunch.sh
+++ tools/makelaunch/makelaunch.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
+
+
+relpath() {
+    local pos="${1%%/}" ref="${2%%/}" down=''
+
+    while :; do
+        test "$pos" = '/' && break
+        case "$ref" in $pos/*) break;; esac
+        down="../$down"
+        pos=${pos%/*}
+    done
+
+    echo "$down${ref##$pos/}"
+}
+
 if [ $# -ne 3 ]
 then
   echo "Usage: makelaunch.sh <project directory path> <elf file path> <\"Eclipse Project Name\">"
   exit 1
 else
+  P1="${1//\\//}"
+  P2="${2//\\//}"
   DEBUGDIR=debug
-  ELFRELPATH=$(realpath --relative-to=$1 $2)
+#  ELFRELPATH=$(realpath --relative-to=$P1 $P2)
+  ELFRELPATH=$(relpath $P1 $P2)
   echo "Creating debug launch configurations for \"$ELFRELPATH\" ..."
-  ELFNAME=$(echo "$ELFRELPATH" | sed -r "s|/|:|g")
+  ELFNAME=$(echo "$ELFRELPATH" | sed -r "s|/|-|g")
   LAUNCHNAME1="$3 ($ELFNAME)(OpenOCD, Flash and Run)"
   LAUNCHNAME2="$3 ($ELFNAME)(OpenOCD, Just Run)"
   mkdir -p $1/$DEBUGDIR
-  cat ./arm_flash-and-run.launch.template | sed -r "s|EEEEEEEE|$2|g" | sed -r "s|PPPPPPPP|$3|g" > "$1/$DEBUGDIR/$LAUNCHNAME1.launch"
-  cat ./arm_just-run.launch.template | sed -r "s|EEEEEEEE|$2|g" | sed -r "s|PPPPPPPP|$3|g" > "$1/$DEBUGDIR/$LAUNCHNAME2.launch"
+  cat ./arm_flash-and-run.launch.template | sed -r "s|EEEEEEEE|$2|g" | sed -r "s|PPPPPPPP|$3|g" > "$P1/$DEBUGDIR/$LAUNCHNAME1.launch"
+  cat ./arm_just-run.launch.template | sed -r "s|EEEEEEEE|$2|g" | sed -r "s|PPPPPPPP|$3|g" > "$P1/$DEBUGDIR/$LAUNCHNAME2.launch"
   echo
   echo "Done"
 fi

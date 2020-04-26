e7c84d9da5d2593bfe7804b57be76c3e436b9dec
roccomarco <roccomarco@27425a3e-05d8-49a3-a47f-9c15f0e5edd8>
2020-04-26 14:25:12 +0000

    Implemented relocation of workspace preferences
    git-svn-id: svn+ssh://svn.osdn.net/svnroot/chibios/branches/chibistudio_trunk@13582 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- tools/relocator/relocator.sh
+++ tools/relocator/relocator.sh
@@ -1,27 +1,51 @@
 #!/bin/bash
+
+
+
+rlctwrksp() {
+  for dir in "$1/workspace"*/
+  do
+    WPREFS="$dir\.metadata\.plugins\org.eclipse.core.runtime\.settings\org.eclipse.core.resources.prefs"
+    echo "Relocating $dir preferences..."
+    if test -f "$WPREFS.bak"; 
+    then
+      echo " - Already relocated... skipping"
+    else
+      mv $WPREFS $WPREFS".bak"
+      RPL=$(echo $1 | sed -e 's|\\|\\\/|g' | sed -e 's|:|\\\\\\:|g')
+      cat "$WPREFS.bak" | sed -r 's|C\\\:\/ChibiStudio|'$RPL'|g' > $WPREFS
+      echo " - Relocated"
+    fi
+  done
+}
 if [ $# -ne 1 ]
 then
   echo "Usage: relocator.sh <chibistudio directory path>"
   exit 1
 else
-  if [ "$1" -ef "C:/ChibiStudio" ]
+## Removing trailing backslash
+  CURRPATH=${1%\\}
+  DEFAULTPATH="C:\ChibiStudio"
+  
+  if [ "$CURRPATH" -ef "DEFAULTPATH" ]
   then
     echo "Default location... skipping"
   else
-    PRFS=$1/eclipse/configuration/.settings/org.eclipse.ui.ide.prefs
-    if test -f "$PRFS.bak"; 
+    echo "Relocating to $CURRPATH..."
+    
+    echo "Relocating Eclipse preferences..."
+    EPREFS="$CURRPATH\eclipse\configuration\.settings\org.eclipse.ui.ide.prefs"
+    if test -f "$EPREFS.bak"; 
     then
-      echo "Already relocated... skipping"
+      echo " - Already relocated... skipping"
     else
-      echo "Relocating to $1..."
-      
-      echo "Relocating Eclipse Workspace preferences..."
-      mv $PRFS $PRFS".bak"
-      RPL=$(echo $1 | sed -e 's|\\|\\\\\\\\|g' | sed -e 's|:|\\\\\\:|g')
-      cat "$PRFS.bak" | sed -r 's|C\\:\\\\ChibiStudio\\\\|'$RPL'|g' > $PRFS
-      echo "Done"
-    fi
+      mv $EPREFS $EPREFS".bak"
+      RPL=$(echo $CURRPATH | sed -e 's|\\|\\\\\\\\|g' | sed -e 's|:|\\\\\\:|g')
+      cat "$EPREFS.bak" | sed -r 's|C\\:\\\\ChibiStudio|'$RPL'|g' > $EPREFS
+      echo " - Relocated"
+    fi     
+    rlctwrksp $CURRPATH
+    echo "Done"
   fi
 fi
-
 exit 0
